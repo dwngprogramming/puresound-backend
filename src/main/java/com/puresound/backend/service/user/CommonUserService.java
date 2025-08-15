97919067d16414cbc1555510f8857a95a5e46ed4
dwngprogramming <dwnq.coding@gmail.com>
2025-08-15 14:07:55 +0700

    refactor(user-service): Rename UserServiceImpl -> CommonUserService and remove unused methods

---
+++ src/main/java/com/puresound/backend/service/user/CommonUserService.java
@@ -0,0 +1,24 @@
+package com.puresound.backend.service.user;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.regex.Pattern;
+
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Service
+@Slf4j
+public class CommonUserService {
+    static Pattern USERNAME_PATTERN = Pattern.compile("^[a-zA-Z0-9\\-_.]{6,30}$");
+    static Pattern EMAIL_PATTERN = Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+
+    public boolean isUsername(String usernameOrEmail) {
+        return usernameOrEmail != null && USERNAME_PATTERN.matcher(usernameOrEmail).matches();
+    }
+
+    public boolean isEmail(String usernameOrEmail) {
+        return usernameOrEmail != null && EMAIL_PATTERN.matcher(usernameOrEmail).matches();
+    }
+}
