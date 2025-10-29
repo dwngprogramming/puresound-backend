83a1b761490842e361349cebc512c14962bc45e6
dwngprogramming <dwnq.coding@gmail.com>
2025-10-29 14:38:15 +0700

    hotfix(cloudinary): Fix error from Cloudinary .env

--- src/main/java/com/puresound/backend/config/CloudinaryConfig.java
+++ src/main/java/com/puresound/backend/config/CloudinaryConfig.java
@@ -1,15 +1,31 @@
 package com.puresound.backend.config;
 
 import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @Configuration
 public class CloudinaryConfig {
 
+    @Value("${cloudinary.cloud-name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api-key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api-secret}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
-        return new Cloudinary(Dotenv.load().get("CLOUDINARY_URL"));
+        return new Cloudinary(
+                ObjectUtils.asMap(
+                        "cloud_name", cloudName,
+                        "api_key", apiKey,
+                        "api_secret", apiSecret
+                )
+        );
     }
 }
