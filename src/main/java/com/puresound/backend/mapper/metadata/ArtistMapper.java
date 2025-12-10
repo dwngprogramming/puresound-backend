53a7e02cbd507ed37de834df467e01a43c593d62
dwngprogramming <dwnq.coding@gmail.com>
2025-12-10 15:01:16 +0700

    [PS-13] feat(featured-artists): add featured artists API endpoint
    - Add new `ArtistApi` controller with endpoint to fetch featured artists with pagination.
    - Improve pagination response by including numberOfElements in mapper.
    - Update documentation and messages for both artist and track APIs.
    - Change version of documentation to 1.0.6

--- src/main/java/com/puresound/backend/mapper/metadata/ArtistMapper.java
+++ src/main/java/com/puresound/backend/mapper/metadata/ArtistMapper.java
@@ -2,9 +2,13 @@ package com.puresound.backend.mapper.metadata;
 
 import com.puresound.backend.dto.metadata.artist.ArtistResponse;
 import com.puresound.backend.dto.metadata.artist.SimplifiedArtistResponse;
+import com.puresound.backend.dto.pagination.PagingResponse;
+import com.puresound.backend.dto.pagination.SPFResponse;
 import com.puresound.backend.entity.jpa.metadata.artist.ArtistMetadata;
 import com.puresound.backend.mapper.GlobalMapperConfig;
 import org.mapstruct.Mapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
@@ -15,4 +19,28 @@ public interface ArtistMapper {
     List<ArtistResponse> toResponses(List<ArtistMetadata> artists);
 
     SimplifiedArtistResponse toSimplifiedResponse(ArtistMetadata artist);
+
+    default SPFResponse<SimplifiedArtistResponse> toSpfSimplifiedResponse(Page<ArtistMetadata> artistMetadataPage) {
+        List<SimplifiedArtistResponse> content = artistMetadataPage
+                .map(this::toSimplifiedResponse)
+                .getContent();
+
+        Sort currentSort = artistMetadataPage.getSort();
+        String sortBy = currentSort.isSorted() ? currentSort.iterator().next().getProperty() : null;
+        String sortDirection = currentSort.isSorted() ? currentSort.iterator().next().getDirection().name() : null;
+
+        PagingResponse pagingResponse = PagingResponse.builder()
+                .page(artistMetadataPage.getNumber() + 1)
+                .size(artistMetadataPage.getSize())
+                .first(artistMetadataPage.isFirst())
+                .last(artistMetadataPage.isLast())
+                .numberOfElements(artistMetadataPage.getNumberOfElements())
+                .totalElements(artistMetadataPage.getTotalElements())
+                .totalPages(artistMetadataPage.getTotalPages())
+                .sortBy(sortBy)
+                .sortDirection(sortDirection)
+                .build();
+
+        return SPFResponse.of(content, pagingResponse);
+    }
 }
